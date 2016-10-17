34d23c0a1bef7926a17bc4ec8e89a8a39bf157f6
leonardo2707 <leonardo_2707@hotmail.com>
2016-10-16 22:34:33 -0200

    Primeiro Comentario de teste

---
+++ src/Main/Main.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Main;
+
+import Funcoes.Grafo; //IMPORTA A FUNÇÃO GRAFO DE OUTRO PACOTE
+import java.util.Scanner;
+
+/**
+ *
+ * @author emerson
+ */
+
+public class Main {
+    public static void main(String[] args){
+        
+        Scanner scan = new Scanner(System.in);
+        
+        System.out.println("----MENU-----");
+        System.out.println("1-Entrada Formal");
+        System.out.println("2-Listar o Grafo");
+        System.out.println("0-Finalizar o programa");
+        int op = scan.nextInt();
+        
+        op=-1;
+        while(op != 0){
+            
+        }
+    }
+}
+
