fca13be71962e5ff495b17bdb37447ff5b883dd4
leonardo2707 <leonardo_2707@hotmail.com>
2016-10-19 10:27:57 -0200

    tornando mais visivel uma matriz

--- src/Main/Menu.java
+++ src/Main/Menu.java
@@ -73,12 +73,12 @@ public class Menu {
       for(i = 0; i < 8; i++)
       {
           
-          for (j = 1; j < 8;j++)
+          for (j = 1; j <= 7;j++)
           {
-              System.out.print("i");
+              System.out.print(" A[" + i + "][" + j + "]");
               
           }
-          System.out.println("i");
+          System.out.println(" A[" + i + "][" + j + "]");
       }
       
         System.out.println();
