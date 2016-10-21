33a6745637ad0e53201b53364604471cf6c1f43a
Eduardo Spillere Anzolin <eduardospillereanzolin@gmail.com>
2016-10-21 11:53:47 -0200

    Continuando organização do projeto para realizar link da interface com o back and, Leo esta precisando de ajuda ainda?

---
+++ src/Main/MainControllerInterface_Back_And_Front.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+*
+*       -----IMPORTANT-------
+*
+*CLASSE QUE REALIZA A LINKAGEM DO BACK AND COM O FRONT, ENVIAMOS OS DADOS 
+*DA INTERFACE GRAFICA PARA ESTA CLASSE E ELA CHAMA OS METODOS NECESSARIOS
+*PARA REALIZAR AS IMPLEMENTAÇÕES
+*
+*
+*       -----IMPORTANT-------
+*/
+package Main;
+
+/**
+ *
+ * @author Eduardo
+ */
+public class MainControllerInterface_Back_And_Front {
+    
+}
