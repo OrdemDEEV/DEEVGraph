08891586ad8a3d18ecee0fad23ddc34daf2227a0
2Dimm <dimitryld@hotmail.com>
2016-11-25 23:35:35 -0200

    AUMENTAR DE NOVO ESSA TELA MALDITA

--- src/Interfaces/Principal.java
+++ src/Interfaces/Principal.java
@@ -78,6 +78,7 @@ public class Principal extends javax.swing.JFrame {
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         setTitle("DEEVgraph");
+        setPreferredSize(new java.awt.Dimension(864, 722));
 
         jPanel1.setBackground(new java.awt.Color(38, 35, 33));
 
